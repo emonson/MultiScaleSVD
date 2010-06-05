df7d26d9359f26e803df35d826306c337fca9448
Eric E Monson <emonson@cs.duke.edu>
2010-06-04 21:23:22 -0400

    Last commit didn't add new hover image test

---
+++ test_mypoints_plot.py
@@ -0,0 +1,63 @@
+# Translated to Python from [VTK]/Charts/Testing/Cxx/TestLinePlot.cxx
+
+import vtk
+from vtk.util import numpy_support as VN
+import math
+import vtkvtg
+from data_source import DataSource
+
+data_file = '/Users/emonson/Data/Fodava/EMoGWDataSets/mnist1_5c_20100324.mat'
+
+# DataSource loads .mat file and can generate data from it for other views
+ds = DataSource(data_file)
+
+# Set up a 2D scene, add an XY chart to it
+view = vtk.vtkContextView()
+view.GetRenderer().SetBackground(1.0, 1.0, 1.0)
+view.GetRenderWindow().SetSize(400, 300)
+
+# Testing my custom chart class which has image hover tooltips
+chart = vtkvtg.vtkMyChartXY()
+view.GetScene().AddItem(chart)
+
+# Create a annotation link to access selection in parallel coordinates view
+annotationLink = vtk.vtkAnnotationLink()
+# If you don't set the FieldType explicitly it ends up as UNKNOWN (as of 21 Feb 2010)
+# See vtkSelectionNode doc for field and content type enum values
+annotationLink.GetCurrentSelection().GetNode(0).SetFieldType(1)     # Point
+annotationLink.GetCurrentSelection().GetNode(0).SetContentType(4)   # Indices
+# Connect the annotation link to the parallel coordinates representation
+chart.SetAnnotationLink(annotationLink)
+
+test_id = 0
+table = ds.GetNodeOneScaleCoeffTable(test_id)
+id_list = ds.PIN[test_id]
+image_stack = ds.GetProjectedImages(id_list)
+
+line1 = chart.AddPlot(1)		# POINTS
+line1.SetInput(table, 0, 2)
+line1.SetMarkerStyle(1)
+line1.SetColor(255, 0, 0, 255)
+
+# Need to set the image stack for the plot which will get resliced 
+chart.GetPlot(0).SetImageStack(image_stack)
+chart.SetTooltipShowImage(True);
+
+# Finally render the scene and compare the image to a reference image
+# view.GetRenderWindow().SetMultiSamples(0)
+
+def selectionCallback(caller, event):
+        annSel = annotationLink.GetCurrentSelection()
+        if annSel.GetNumberOfNodes() > 0:
+                idxArr = annSel.GetNode(0).GetSelectionList()
+                if idxArr.GetNumberOfTuples() > 0:
+                        print VN.vtk_to_numpy(idxArr)
+
+
+annotationLink.AddObserver("AnnotationChangedEvent",selectionCallback)
+
+# view.ResetCamera()
+view.Render()
+
+# Start interaction event loop
+view.GetInteractor().Start()
