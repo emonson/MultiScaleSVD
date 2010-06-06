3077e2e93f78b858b7a7621cfc62b9e7eb2d9f4b
Eric E. Monson <emonson@LaPommeWideBoy.local>
2010-06-06 10:24:41 -0400

    Beginnings of indexing plot points and trial of 2x image size

--- test_mypoints_plot.py
+++ test_mypoints_plot.py
@@ -12,8 +12,7 @@ data_file = '/Users/emonson/Data/Fodava/EMoGWDataSets/mnist1_5c_20100324.mat'
 ds = DataSource(data_file)
 
 # Set up a 2D scene, add an XY chart to it
-view = vtk.vtkContextView()
-view.GetRenderer().SetBackground(1.0, 1.0, 1.0)
+view = vtkvtg.vtkMyContextView()
 view.GetRenderWindow().SetSize(400, 300)
 
 # Testing my custom chart class which has image hover tooltips
@@ -29,19 +28,19 @@ annotationLink.GetCurrentSelection().GetNode(0).SetContentType(4)   # Indices
 # Connect the annotation link to the parallel coordinates representation
 chart.SetAnnotationLink(annotationLink)
 
-test_id = 0
+test_id = 68
 table = ds.GetNodeOneScaleCoeffTable(test_id)
 id_list = ds.PIN[test_id]
 image_stack = ds.GetProjectedImages(id_list)
 
 line1 = chart.AddPlot(1)		# POINTS
-line1.SetInput(table, 0, 2)
-line1.SetMarkerStyle(1)
+line1.SetInput(table, 0, 1)
+line1.SetMarkerStyle(2)
 line1.SetColor(255, 0, 0, 255)
 
 # Need to set the image stack for the plot which will get resliced 
 chart.GetPlot(0).SetImageStack(image_stack)
-chart.SetTooltipShowImage(True);
+chart.SetTooltipShowImage(True)
 
 # Finally render the scene and compare the image to a reference image
 # view.GetRenderWindow().SetMultiSamples(0)
